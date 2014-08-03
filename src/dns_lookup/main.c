7d031daa781789e902b1bb3bff1eff2b2dfe74a8
Tomas Korcak <korczis@gmail.com>
2014-08-03 00:29:20 -0700

    Parametrized - can be used as dns_lookup "seznam.cz"

--- src/dns_lookup/main.c
+++ src/dns_lookup/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>    // strcpy
+#include <netdb.h>     // gethostbyname
+#include <arpa/inet.h>
 
 int main(int argc, char** argv)
 {
@@ -8,6 +11,29 @@ int main(int argc, char** argv)
 		fprintf(stderr, "%s\n", "No hostname specified!");
 		return EXIT_FAILURE;
 	}
+
+	// First argument is hostname to look for
+	const char* arg_host = argv[1];
+
+	struct hostent *h = gethostbyname(arg_host);
+	if(0 == h)
+	{
+		fprintf(stderr, "Unable to resolve host '%s'\n", arg_host);
+		return EXIT_FAILURE;
+	}
+
+	struct in_addr **addr_list = (struct in_addr **) h->h_addr_list;
+     
+    char ip[INET6_ADDRSTRLEN + 1];
+    ip[INET6_ADDRSTRLEN] = '\0';
+
+    for(int i = 0; addr_list[i] != NULL; i++) 
+    {
+        //Return the first one;
+        strcpy(ip, inet_ntoa(*addr_list[i]));
+        fprintf(stdout, "%s\n", ip);
+        return 0;
+    }
 
 	return EXIT_SUCCESS;      // Declared in <stdlib.h>
 }
