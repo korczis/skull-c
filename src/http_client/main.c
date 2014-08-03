dc439a5eedec905acddb63b8d09c13b5b9e6122e
Tomas Korcak <korczis@gmail.com>
2014-08-03 03:11:38 -0700

    Simple http client

--- src/http_client/main.c
+++ src/http_client/main.c
@@ -18,14 +18,130 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>    // strcpy
+#include <unistd.h>    // close
+#include <netdb.h>     // gethostbyname
+#include <arpa/inet.h>
 
 int main(int argc, char** argv)
 {
-	const char* name = argc > 1 ? argv[1] : "Travis";
+	const char* prefix = "http://";
+	const char* url = argc > 1 ? argv[1] : "http://google.com";
 
-	fprintf(stdout, "Hello World %s!\n", name); // Declared in <stdio.h>
+	if(strstr(url, prefix) == 0)
+	{
+		fprintf(stderr, "Invalid URL '%s', must start with '%s'\n", url, prefix);
+		return EXIT_FAILURE;
+	}
+
+	url = url + strlen(prefix);
+
+	const char* path = strstr(url, "/");
+	int hostname_len = 0;
+	if(!path)
+	{
+		hostname_len = strlen(url);
+		path = "/";
+	} 
+	else
+	{
+		hostname_len = path - url;
+	}
+
+	char hostname[hostname_len + 1];
+	hostname[hostname_len] = '\0';
+
+	strncpy(hostname, url, hostname_len);
+
+	// Print information about host and path
+	// fprintf(stdout, "HOST: %s\n", hostname);
+	// fprintf(stdout, "PATH %s\n", path);
+
+	// Try to resolve name
+	struct hostent* h = gethostbyname(hostname);
+	if(0 == h || h->h_addr_list[0] == 0)
+	{
+		fprintf(stderr, "Unable to resolve host '%s'\n", hostname);
+		return EXIT_FAILURE;
+	}
+
+	// Try to open socket
+	int s = socket(AF_INET, SOCK_STREAM, 0);
+	if(0 == s)
+	{
+		fprintf(stderr, "Unable to open socket, reason: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	struct sockaddr_in servaddr, cliaddr;
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = *(in_addr_t*)h->h_addr_list[0];
+	servaddr.sin_port = htons(80);
+
+	int res = connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	if(0 != res)
+	{
+		fprintf(stderr, "Unable to connect to host, reason: %s\n", strerror(errno));
+		close(s);
+		return EXIT_FAILURE;
+	}
+
+	// Dynamically allocate memory
+	size_t buffer_size = 256 * 1024;
+	char* buffer = (char*) malloc(buffer_size);
+
+	// Zero buffer memory area
+	memset(buffer, 0, buffer_size);
+
+	// Construct payload
+	const int real_size = snprintf(buffer, buffer_size, "GET %s HTTP/1.0\n\n", path);
+	// fprintf(stdout, "PAYLOAD: %s\n", buffer);
+
+	res = sendto(s, buffer, real_size, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	if(res != real_size) 
+	{
+		fprintf(stderr, "Unable to send request\n");
+		
+		// Free allocated memory
+		free(buffer);
+		buffer = 0;
+
+		// Close socket
+		close(s);
+		s = 0;
+
+		return EXIT_FAILURE;
+	}
+
+	// Print RESPONSE to console
+	// fprintf(stdout, "RESPONSE:\n");
 	
+	// Process received bytes
+	int received_bytes = 0;
+	do
+    {
+		received_bytes = recvfrom(s, buffer, buffer_size - 1, 0, NULL, NULL);
+		if(received_bytes > 0)
+		{
+			buffer[received_bytes] = '\0';
+			fprintf(stdout, "%s", buffer);	
+		}
+	} while (received_bytes > 0);
+
+	// Add trailing newline
+	fprintf(stdout, "\n");
+
+	// Free allocated memory
+	free(buffer);
+	buffer = 0;
+
+	// Close socket
+	close(s);
+	s = 0;
+
 	return EXIT_SUCCESS;      // Declared in <stdlib.h>
 }
