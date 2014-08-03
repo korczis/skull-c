a239da9b8ab4d2475f02ecc29b0b31f00917db7d
Tomas Korcak <korczis@gmail.com>
2014-08-03 00:38:34 -0700

    Cosmetics

--- src/dns_lookup/main.c
+++ src/dns_lookup/main.c
@@ -1,3 +1,23 @@
+// Copyright, 2013-2014, by Tomas Korcak. <korczis@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included inso
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>    // strcpy
@@ -15,14 +35,14 @@ int main(int argc, char** argv)
 	// First argument is hostname to look for
 	const char* arg_host = argv[1];
 
-	struct hostent *h = gethostbyname(arg_host);
+	struct hostent* h = gethostbyname(arg_host);
 	if(0 == h)
 	{
 		fprintf(stderr, "Unable to resolve host '%s'\n", arg_host);
 		return EXIT_FAILURE;
 	}
 
-	struct in_addr **addr_list = (struct in_addr **) h->h_addr_list;
+	struct in_addr** addr_list = (struct in_addr **) h->h_addr_list;
      
     char ip[INET6_ADDRSTRLEN + 1];
     ip[INET6_ADDRSTRLEN] = '\0';
