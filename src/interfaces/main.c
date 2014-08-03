c33f541ba47b2b7e8030461bda94b88f30568260
Tomas Korcak <korczis@gmail.com>
2014-08-03 13:05:16 -0700

    Working interfaces example

--- src/interfaces/main.c
+++ src/interfaces/main.c
@@ -19,14 +19,68 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <errno.h>
+#include <ifaddrs.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>    // strcpy
+#include <strings.h>
+#include <unistd.h>    // close
+#include <netdb.h>     // gethostbyname
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <net/ethernet.h>
 
 int main(int argc, char** argv)
 {
-	const char* name = argc > 1 ? argv[1] : "Travis";
+	struct ifaddrs *ifaddr, *ifa;
+	int family, s, n;
+	char host[NI_MAXHOST];
 
-	fprintf(stdout, "Hello World %s!\n", name); // Declared in <stdio.h>
-	
+	if (getifaddrs(&ifaddr) == -1) {
+		fprintf(stderr, "Unable to get info about interfaces, reason: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	/* Walk through linked list, maintaining head pointer so we
+	can free list later */
+
+	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++)
+	{
+		if (ifa->ifa_addr == NULL)
+		{
+			continue;
+		}
+
+		int family = ifa->ifa_addr->sa_family;
+
+		// Display interface name and family (including symbolic form of the latter for the common families)
+
+		printf("%-8s %s (%d)\n",
+			ifa->ifa_name,
+			(family == AF_INET) ? "AF_INET" :
+			(family == AF_INET6) ? "AF_INET6" : "???",
+			family);
+
+		if (family == AF_INET || family == AF_INET6)
+		{
+			s = getnameinfo(ifa->ifa_addr, (family == AF_INET) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+			
+			if (s != 0)
+			{
+				printf("getnameinfo() failed: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+
+			printf("\t\taddress: <%s>\n", host);
+
+		}
+	}
 	return EXIT_SUCCESS;      // Declared in <stdlib.h>
 }
