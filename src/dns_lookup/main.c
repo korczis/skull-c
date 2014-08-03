92c52c75396ac2aaf80b794b81218295bdefae1d
Tomas Korcak <korczis@gmail.com>
2014-08-03 01:49:17 -0700

    More commented sources

--- src/dns_lookup/main.c
+++ src/dns_lookup/main.c
@@ -26,6 +26,7 @@
 
 int main(int argc, char** argv)
 {
+	// Check if there was any hostname specified on cmd-line
 	if(argc < 2) 
 	{
 		fprintf(stderr, "%s\n", "No hostname specified!");
@@ -35,6 +36,7 @@ int main(int argc, char** argv)
 	// First argument is hostname to look for
 	const char* arg_host = argv[1];
 
+	// Try to resolve hostname specified on cmd-line
 	struct hostent* h = gethostbyname(arg_host);
 	if(0 == h)
 	{
@@ -42,18 +44,38 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	struct in_addr** addr_list = (struct in_addr **) h->h_addr_list;
-     
-    char ip[INET6_ADDRSTRLEN + 1];
+	// Check if there are any aliases and if so print header - Alias(es)
+	if(h->h_aliases[0] != 0) 
+	{
+		fprintf(stdout, "Alias(es)\n");
+	}
+
+	// Print all aliases
+	int i = 0;
+	for(char* alias = h->h_aliases[i]; alias != NULL; i++)
+	{
+		fprintf(stdout, "%s\n", alias);
+	}
+
+	// Check if there are any IPs and if so print header - IP(s)
+    if(h->h_addr_list[0] != 0)
+    {
+		fprintf(stdout, "IP(s)\n");
+	}
+
+	// Allocate space for IP converted to string
+	char ip[INET6_ADDRSTRLEN + 1];
     ip[INET6_ADDRSTRLEN] = '\0';
 
-    for(int i = 0; addr_list[i] != NULL; i++) 
+	// Print all IPs
+    struct in_addr** addr_list = (struct in_addr **) h->h_addr_list;
+    for(i = 0; addr_list[i] != NULL; i++) 
     {
         //Return the first one;
         strcpy(ip, inet_ntoa(*addr_list[i]));
         fprintf(stdout, "%s\n", ip);
-        return 0;
     }
 
+    // End with success error code - 0
 	return EXIT_SUCCESS;      // Declared in <stdlib.h>
 }
